6d9a1810a4e6480cdf6e942ec61bef3ecfd69936
thw4514 <prasanna.anbazhagan@kroger.com>
2019-11-04 19:02:01 +0530

    Added Rest API Module

---
+++ restApi/src/main/kotlin/com/example/restapi/Main.kt
@@ -0,0 +1,29 @@
+/**
+ * Created by Prasanna on 6/3/18.
+ *
+ * @author Prasanna <praslnx8@gmail.com>
+ * @version 1.0
+ */
+
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }
+    server.start(wait = true)
+}
